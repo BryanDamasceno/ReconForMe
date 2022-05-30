891df2f3385ce4b7764b34813c85f6e24a89993c
Bryan Damasceno <bryan42damasceno@gmail.com>
2022-05-30 20:53:43 -0300

    adding subfinder

--- ReconForMe.py
+++ ReconForMe.py
@@ -1,29 +1,46 @@
 import os
 
-print("Hello, Bryan.")
-print("Who is your target? ", end=" ")
+#===============FUNCTIONS===============
+def callSubfinder(target):
+   os.system("echo 'SUBDOMAINS OF YOUR TARGET: ' > ./report/enumeration_result.txt")
+   subfinder = "subfinder -d " + target + " >> ./report/enumeration_result.txt" 
+   return subfinder
 
+def callNmap(target):
+   os.system("echo 'SCAN OF YOUR TARGET: ' > ./report/scan_result.txt")
+
+   print("Agressive?", end=" ")
+   answer = input() 
+   answer = answer.lower()
+   check = True
+
+   if answer == "no"  or answer == "n":
+      check = False
+      nmap = "sudo nmap " + target + " >> ./report/scan_result.txt" 
+      return nmap
+   
+   if answer == "yes"  or answer == "y":
+      check = False
+      nmap = "sudo nmap -v -A " + target + " >> ./report/scan_result.txt"
+      return nmap
+
+   if check == True:    
+      print("Please say yes or no, sir")
+      callNmap(target)
+
+
+if not os.path.isdir('./report'): os.system("mkdir ./report")
+   
+
+print("Hello, Bryan.")
+print("Who is your target?", end=" ")
 target = input()
 
-os.system("mkdir ./report")
-os.system("echo 'SCAN OF YOUR TARGET: ' > ./report/scan_result")
 
-print("Agressive?", end=" ")
-answer = input()
-answer = answer.lower()
-check = True
 
-if answer == "no"  or answer == "n":
-   nmap = "sudo nmap " + target + " >> ./report/scan_result"
-   check = False 
- 
-if answer == "yes"  or answer == "y":
-   nmap = "sudo nmap -v -A " + target + " >> ./report/scan_result"
-   check = False
 
-if check == True:    
-    print("Please say yes or no, sir")
 
-print(nmap)
 
-os.system(nmap)
+os.system(callNmap(target))
+os.system(callSubfinder(target))
+print("Scanning...")
